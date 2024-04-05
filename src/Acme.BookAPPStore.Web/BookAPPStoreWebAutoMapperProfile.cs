84a57c737f6e44d090b2641c80859d9cacccb372
Ahmed Abdallah <ahmedismail14141414@gmail.com>
2024-04-05 16:07:08 +0200

    Add project files.

---
+++ src/Acme.BookAPPStore.Web/BookAPPStoreWebAutoMapperProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace Acme.BookAPPStore.Web;
+
+public class BookAPPStoreWebAutoMapperProfile : Profile
+{
+    public BookAPPStoreWebAutoMapperProfile()
+    {
+        //Define your AutoMapper configuration here for the Web project.
+    }
+}
